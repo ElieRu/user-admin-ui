ed52eb78001b1fab16f1f690e2de754c5ef54368
RUHAMYA Elie <elieruhamya20@gmail.com>
2024-03-18 08:08:20 +0200

    the imlplementation of blank page

--- src/views/BlankPage.vue
+++ src/views/BlankPage.vue
@@ -1,6 +1,52 @@
-<!-- eslint-disable vue/multi-word-component-names -->
 <template>
-    <div>
-        blankpage
+  <div style="width: 100%; height: 100%; position: fixed">
+    <MyHeader></MyHeader>
+
+    <MyNotice></MyNotice>
+
+    <div
+      class="container-fluid d-flex justify-content-center"
+      style="position: fixed; height: 87%; overflow: visible; padding-right: 0px; padding-left: 0px"
+    >
+      <div class="row" style="width: 100%; /*margin: auto;*/">
+        <MyNavbar :my_router="my_router"></MyNavbar>
+
+        <div class="col-lg-9" style="overflow: auto; height: 100%; padding-top: 25px">
+          <div class="row d-block" style="height: 100%">
+            <div class="col">
+              <h1 class="mt-2 mb-4">Blank page</h1>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+
+    <MyMenu :my_router="my_router"></MyMenu>
+  </div>
 </template>
+
+<script>
+import MyHeader from '@/components/Header.vue'
+import MyNotice from '@/components/Notice.vue'
+import MyNavbar from '@/components/NavBar.vue'
+import MyMenu from '@/components/Menu.vue'
+import router from '@/router'
+
+export default {
+  data() {
+    return {
+      my_router: ''
+    }
+  },
+  components: {
+    MyHeader,
+    MyNavbar,
+    MyNotice,
+    MyMenu
+  },
+
+  mounted() {
+    this.my_router = router.currentRoute.value.name
+  }
+}
+</script>
