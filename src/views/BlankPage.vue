c1a6c1be97d1458f889bd6173c2c417ac91fedd2
RUHAMYA Elie <163524403+ElieRu@users.noreply.github.com>
2024-03-18 18:20:11 +0200

    Update BlankPage.vue
    update blank page

--- src/views/BlankPage.vue
+++ src/views/BlankPage.vue
@@ -15,6 +15,21 @@
           <div class="row d-block" style="height: 100%">
             <div class="col">
               <h1 class="mt-2 mb-4">Blank page</h1>
+
+              <div class="dropdown">
+                <button
+                  class="btn btn-primary dropdown-toggle"
+                  aria-expanded="false"
+                  data-bs-toggle="dropdown"
+                  type="button"
+                ></button>
+                <div class="dropdown-menu">
+                  <a class="dropdown-item" href="#">First Item</a
+                  ><a class="dropdown-item" href="#">Second Item</a
+                  ><a class="dropdown-item" href="#">Third Item</a>
+                </div>
+              </div>
+              
             </div>
           </div>
         </div>
